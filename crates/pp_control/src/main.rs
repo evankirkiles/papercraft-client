77547e58f09e3ec95a7b8af81ba4195bf15ba63b
Evan Kirkiles <kirkilese@gmail.com>
2025-03-16 07:40:51 -0400

    Deleted old pp_control

--- crates/pp_control/src/main.rs
+++
@@ -1,7 +0,0 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let event_loop = winit::event_loop::EventLoop::with_user_event().build()?;
-    event_loop.set_control_flow(winit::event_loop::ControlFlow::Poll);
-    let mut app = pp_control::App::new(event_loop.create_proxy());
-    event_loop.run_app(&mut app)?;
-    Ok(())
-}
